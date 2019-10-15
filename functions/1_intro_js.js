74573e5e14fb533d424feddf6b061cc73b31401e
Ayako <ayakosayama1987@gmail.com>
2019-10-15 13:58:20 -0700

    update 1st day

---
+++ functions/1_intro_js.js
@@ -0,0 +1,15 @@
+
+//standard javascript
+var name = "Ayako"
+console.log("1:name", name);
+
+name = "Ali"
+console.log("1:name", name);
+
+
+//ES6
+let animal = "cat"
+console.log("1:animal", animal);
+
+animal = "dog"
+console.log("1:animal", animal);
