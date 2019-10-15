9769d8839f549be79989c8b96bb424682fb3f6e7
Ayako <ayakosayama1987@gmail.com>
2019-10-15 15:39:12 -0700

    1_intro update

--- functions/1_intro_js.js
+++ functions/1_intro_js.js
@@ -13,3 +13,55 @@ console.log("1:animal", animal);
 
 animal = "dog"
 console.log("1:animal", animal);
+
+const age = 31
+console.log("1:age", age);
+//can't do this
+// age = 40;
+
+
+const count = "1"
+const updatedCount = parseInt(count);
+console.log("2:number", updatedCount + 1)
+
+// const str = toString(updatedCount).substring;
+// console.log("2:string", str);
+
+console.log(`3:template literals My age is ${age}`);
+
+const students = ["Vlad", "Isao", "Shin", "Cedric", "Risa", "Nobu", "Tatsuya", "Paulo", `Newaj`, "Ronan", "Gustavo", 'Gustavo', "Jun", "James", "Marcus"]
+const [firstStudent, secondStudent] = students;
+console.log("4:destructuring", firstStudent, secondStudent);
+
+const ayakoObj = {
+  name: "Ayako Sayama",
+  age: 31,
+  profession: "Frontend developer",
+  pets: ["cat", "dog"]
+}
+
+const jobTitle = "profession"
+
+console.log("5:dot notation", ayakoObj.profession)
+console.log("5:square brackets notation", ayakoObj['profession']);
+console.log("5:square brackets notation with variable", ayakoObj[jobTitle])
+
+const keyName = "webDev";
+
+let classes = {
+  [keyName]: "This is webdev"
+}
+
+console.log(classes[keyName]);
+console.log(classes.webDev)
+
+const { pets } = ayakoObj;
+console.log("6:pets", pets);
+console.log('ayakoObj', ayakoObj)
+
+
+const number1 = 3;
+const number2 = 5;
+
+const result = number1 === number2 ? "number 1 is same as number 2" : "number 1 is not number 2";
+console.log("7:conditional", result);
